debca8a4f82a8ddcfc1b35576e8b3dafbb6473fe
chigivera <ayman.benchamkha@gmail.com>
2024-12-04 19:08:25 +0000

    commit

--- api/main.py
+++ api/main.py
@@ -9,8 +9,9 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*","http://localhost:3000","https://localhost:3000"],
     allow_credentials=True,
+    allow_origin_regex=".*",
     allow_methods=["*"],
     allow_headers=["*"],
 )
